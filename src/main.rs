d96602bbaa277a9b9b58da18e7165beec6dc50eb
Clément Jacquemaire <clement@qevlar.com>
2025-01-17 22:48:14 +0100

    Finish day 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use actix_web::{get, web::{ServiceConfig,Query}, Result, HttpResponse, http::header};
-use shuttle_actix_web::ShuttleActixWeb;
+use actix_web::{
+    get,
+    http::header,
+    web::{Query, ServiceConfig},
+    HttpResponse, Result,
+};
 use serde::Deserialize;
+use shuttle_actix_web::ShuttleActixWeb;
 use std::net::{Ipv4Addr, Ipv6Addr};
 
 mod ip_utils;
@@ -15,54 +20,45 @@ async fn hello_bird() -> &'static str {
 #[get("/-1/seek")]
 async fn seek() -> HttpResponse {
     HttpResponse::Found()
-        .append_header((header::LOCATION, "https://www.youtube.com/watch?v=9Gc4QTqslN4"))
+        .append_header((
+            header::LOCATION,
+            "https://www.youtube.com/watch?v=9Gc4QTqslN4",
+        ))
         .finish()
 }
 
 #[derive(Deserialize)]
-struct DestV4Query {
-    from: Ipv4Addr,
-    key: Ipv4Addr,
+struct DestQuery<T> {
+    from: T,
+    key: T,
 }
 
 #[derive(Deserialize)]
-struct KeyV4Query {
-    from: Ipv4Addr,
-    to: Ipv4Addr,
+struct KeyQuery<T> {
+    from: T,
+    to: T,
 }
 
-#[derive(Deserialize)]
-struct DestV6Query {
-    from: Ipv6Addr,
-    key: Ipv6Addr,
-}
-
-#[derive(Deserialize)]
-struct KeyV6Query {
-    from: Ipv6Addr,
-    to: Ipv6Addr,
-}
-
-#[get("/2/dest")] 
-async fn dest_v4(query: Query<DestV4Query>) -> Result<String> {
+#[get("/2/dest")]
+async fn dest_v4(query: Query<DestQuery<Ipv4Addr>>) -> Result<String> {
     let result = ip_utils::add_ip_addresses(query.from, query.key);
     Ok(format!("{}", result))
 }
 
-#[get("/2/key")] 
-async fn key_v4(query: Query<KeyV4Query>) -> Result<String> {
-    let result = ip_utils::subtract_ip_addresses(query.from, query.to);
+#[get("/2/key")]
+async fn key_v4(query: Query<KeyQuery<Ipv4Addr>>) -> Result<String> {
+    let result = ip_utils::subtract_ip_addresses(query.to, query.from);
     Ok(format!("{}", result))
 }
 
 #[get("/2/v6/dest")]
-async fn dest_v6(query: Query<DestV6Query>) -> Result<String> {
+async fn dest_v6(query: Query<DestQuery<Ipv6Addr>>) -> Result<String> {
     let result = ip_utils::xor_ipv6_addresses(query.from, query.key);
     Ok(format!("{}", result))
 }
 
 #[get("/2/v6/key")]
-async fn key_v6(query: Query<KeyV6Query>) -> Result<String> {
+async fn key_v6(query: Query<KeyQuery<Ipv6Addr>>) -> Result<String> {
     let result = ip_utils::xor_ipv6_addresses(query.from, query.to);
     Ok(format!("{}", result))
 }
@@ -70,7 +66,12 @@ async fn key_v6(query: Query<KeyV6Query>) -> Result<String> {
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_bird).service(seek).service(dest_v4).service(key_v4).service(dest_v6).service(key_v6);
+        cfg.service(hello_bird)
+            .service(seek)
+            .service(dest_v4)
+            .service(key_v4)
+            .service(dest_v6)
+            .service(key_v6);
     };
 
     Ok(config.into())
